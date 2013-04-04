59d00bc8279757e88cecbadf669cf31c547867db
Michael Schröder <mc.schroeder@gmail.com>
2013-04-04 22:06:50 +0200

    Isolate credentials from example.hs

--- example.hs
+++ example.hs
@@ -1,30 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Web.Yelp
+import qualified Web.Yelp as Y
 
 import Network.HTTP.Conduit (withManager)
 import Control.Monad.IO.Class (liftIO)
 
-creds = Credentials { consumerKey    = "your"
-                    , consumerSecret = "secret"
-                    , token          = "api"
-                    , tokenSecret    = "key" 
-                    }
-
 main :: IO ()
-main = withManager $ \manager ->
-    runYelpT creds manager $ do
-        --res <- search [("term",Just "food"),("location",Just "San Francisco")]
-        --res <- search (NeighbourhoodQuery (Neighbourhood "Wien" Nothing))
-        --               Nothing
-        --               (Just (Paging { pagingLimit = 20, pagingOffset = 0 }))
-        --               SortByMatch
-        --               SearchFilter { filterCategories = []
-        --                            , filterRadius     = Nothing
-        --                            , filterDeals      = False
-        --                            }
-        --               (Just (Locale { countryCode = "AT", language = "de" 
-        --                             , filterByLanguage = False
-        --                             }))
-        res <- getBusiness "yelp-san-francisco" Nothing
-        liftIO $ print res
+main = do
+    credentials <- read `fmap` readFile "credentials"
+    --let credentials = Y.Credentials "cKey" "cSecret" "token" "tokenSecret"
+    withManager $ \manager ->
+        Y.runYelpT credentials manager $ do
+            results <- Y.simpleSearch "vienna" (Just "schnitzel")
+            liftIO $ print results
